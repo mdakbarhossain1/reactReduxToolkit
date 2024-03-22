b6e002a56c98fb0ac5f6e038ce645c2d91864e7f
mdakbarhossain1 <akbarshovo18@gmail.com>
2024-03-22 22:17:01 +0600

    new Refresh project for RTQ react redux queraty

--- src/redux/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice.js"
-
-
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-    }
-})
